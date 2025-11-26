27fae20d4ba8d14d03c6faef6317d0c6a5d898cd
Ryan Lindsey <ryan@rynsy.com>
2025-11-26 17:07:52 -0500

    getters/setters for registers, read/write for memory bus, missing register
    Added getters/setters for all registers and a missing bitfield for the
    interrupt controller. Some comments for organization. Need to implement
    tests around register handling. Next step is to work on boot-state and
    implementing methods in the test harness.

--- include/gb/gb_core.h
+++ include/gb/gb_core.h
@@ -4,6 +4,9 @@
 // NOTE: Anything needed by C++ needs to be wrapped in extern "C" for the linker
 // to resolve references.
 //
+// For this project, the only C++ will be the ImGUI debug interface for
+// development The core will be plain C
+//
 #ifdef __cplusplus
 extern "C" {
 #endif
@@ -13,9 +16,9 @@ extern "C" {
 #include <stdint.h>
 
 // Constants
-#define MAX_ROM_SIZE 8 * 1024 * 1024 // 8 MB
-#define WORK_RAM_SIZE_BYTES 8 * 1024 // 8 KB
-#define HIGH_RAM_SIZE_BYTES 127      // 127 B
+#define MAX_ROM_SIZE (8 * 1024 * 1024) // 8 MB
+#define WORK_RAM_SIZE_BYTES (8 * 1024) // 8 KB
+#define HIGH_RAM_SIZE_BYTES 127        // 127 B
 
 // Types
 
@@ -52,12 +55,16 @@ typedef struct {
   uint8_t d, e;
   uint8_t h, l;
 
+  // pointers
   uint16_t sp;
   uint16_t pc;
 
   // memory maps
   uint8_t wram[WORK_RAM_SIZE_BYTES];
   uint8_t hram[HIGH_RAM_SIZE_BYTES];
+
+  // interrupt register, mapped to 0xFFFF
+  uint8_t ie;
 
   // flags
   bool ime;
@@ -66,13 +73,42 @@ typedef struct {
 
 } gb_core;
 
-// Functions
+/* Functions */
 
+// Core functions
 int gb_core_version(void);
 void gb_init(gb_core *gb);
 void gb_load_rom(gb_core *gb, const char *path);
 void gb_step(gb_core *gb);
 void gb_run_frame(gb_core *gb);
+
+// Memory Bus
+uint8_t gb_read8(gb_core *gb, uint16_t addr);
+bool gb_write8(gb_core *gb, uint16_t addr, uint8_t value);
+
+// 16-bit Register views
+uint16_t get_AF(gb_core *gb);
+uint16_t get_BC(gb_core *gb);
+uint16_t get_DE(gb_core *gb);
+uint16_t get_HL(gb_core *gb);
+
+// 16-bit Register setters
+void set_AF(gb_core *gb, uint16_t value);
+void set_BC(gb_core *gb, uint16_t value);
+void set_DE(gb_core *gb, uint16_t value);
+void set_HL(gb_core *gb, uint16_t value);
+
+// 1-bit Register views
+bool get_Z(gb_core *gb);
+bool get_N(gb_core *gb);
+bool get_H(gb_core *gb);
+bool get_C(gb_core *gb);
+
+// 1-bit Register setters
+void set_Z(gb_core *gb, bool value);
+void set_N(gb_core *gb, bool value);
+void set_H(gb_core *gb, bool value);
+void set_C(gb_core *gb, bool value);
 
 // Helpers
 uint8_t *read_binary_file(const char *path, uint32_t *out_size);
--- src/core/gb_core.c
+++ src/core/gb_core.c
@@ -1,9 +1,11 @@
 #include "gb/gb_core.h"
-#include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
+/* Functions */
+
+// Core functions
 int gb_core_version(void) { return 1; }
 
 void gb_init(gb_core *gb) {
@@ -28,8 +30,146 @@ void gb_load_rom(gb_core *gb, const char *path) {
   gb->cart.rom_size = rom_size;
   memcpy(gb->cart.rom, rom, rom_size);
   free(rom);
+
+  //  0x0147 in the ROM contains the MBC type of the ROM, TODO: double-check,
+  //  set MBC accordingly
+  //  gb->cart.mbc_type = gb->cart.rom[0x0147];
+  gb->cart.mbc_type = 0;
 }
 
+//
+//  Fetch opcode at PC
+//  Increment PC
+//  Decode/Execute Instruction
+//  return cycles consumed for instruction
+//
+void gb_step(gb_core *gb) { printf("Taking a step....\n"); }
+
+//
+//  Accumulate cycles until you've executed one frames worth
+//  ~ 70224 CPU Cycles per frame (~4.19MHz/59.7Hz)
+//  Step CPU
+//  Step PPU with cycle const
+//  Step timers, handle interrupts
+//
+void gb_run_frame(gb_core *gb) { printf("Running a frame....\n"); }
+
+// Memory Bus
+uint8_t gb_read8(gb_core *gb, uint16_t addr) {
+
+  if (addr <= 0x7FFF && addr < gb->cart.rom_size) {
+    return gb->cart.rom[addr];
+  } else if (0xC000 <= addr && addr <= 0xDFFF) {
+    // read WRAM
+    return gb->wram[addr - 0xC000];
+  } else if (0xE000 <= addr && addr <= 0xFDFF) {
+    // echo of 0xC000-0xDDFF
+    return gb->wram[addr - 0x2000];
+  } else if (0xFF80 <= addr && addr <= 0xFFFE) {
+    // read HRAM
+    return gb->hram[addr - 0xFF80];
+  } else if (0xFFFF == addr) {
+    return gb->ie;
+  } else {
+    // Invalid memory access
+    return 0xFF;
+  }
+}
+bool gb_write8(gb_core *gb, uint16_t addr, uint8_t value) {
+  bool success = false;
+  if (0xC000 <= addr && addr <= 0xDFFF) {
+    // read WRAM
+    gb->wram[addr - 0xC000] = value;
+    success = true;
+  } else if (0xE000 <= addr && addr <= 0xFDFF) {
+    // echo of 0xC000-0xDDFF
+    gb->wram[addr - 0x2000] = value;
+    success = true;
+  } else if (0xFF80 <= addr && addr <= 0xFFFE) {
+    // read HRAM
+    gb->hram[addr - 0xFF80] = value;
+    success = true;
+  } else if (0xFFFF == addr) {
+    gb->ie = value;
+    success = true;
+  }
+  return success;
+}
+
+// 16-bit Register views
+uint16_t get_AF(gb_core *gb) { return (gb->a) << 8 | (gb->f & 0xF0); }
+uint16_t get_BC(gb_core *gb) { return (gb->b) << 8 | gb->c; }
+uint16_t get_DE(gb_core *gb) { return (gb->d) << 8 | gb->e; }
+uint16_t get_HL(gb_core *gb) { return (gb->h) << 8 | gb->l; }
+
+// 16-bit Register setters
+void set_AF(gb_core *gb, uint16_t value) {
+  gb->a = (uint8_t)((value >> 8) & 0xFF);
+  gb->f = (uint8_t)value & 0xF0; // Lower nibble of F must be 0
+}
+void set_BC(gb_core *gb, uint16_t value) {
+  gb->b = (uint8_t)((value >> 8) & 0xFF);
+  gb->c = (uint8_t)value & 0xFF;
+}
+void set_DE(gb_core *gb, uint16_t value) {
+  gb->d = (uint8_t)((value >> 8) & 0xFF);
+  gb->e = (uint8_t)value & 0xFF;
+}
+void set_HL(gb_core *gb, uint16_t value) {
+  gb->h = (uint8_t)((value >> 8) & 0xFF);
+  gb->l = (uint8_t)value & 0xFF;
+}
+
+// 1-bit Register views
+// Upper 4 bits of F are used for flags, lower 4 always 0
+// Zero Flag
+bool get_Z(gb_core *gb) { return (gb->f & (1 << 7)) != 0; }
+// Subtraction Flag
+bool get_N(gb_core *gb) { return (gb->f & (1 << 6)) != 0; }
+// Half-Carry Flag
+bool get_H(gb_core *gb) { return (gb->f & (1 << 5)) != 0; }
+// Carry Flag
+bool get_C(gb_core *gb) { return (gb->f & (1 << 4)) != 0; }
+
+// 1-bit Register setters
+// Zero Flag
+void set_Z(gb_core *gb, bool value) {
+  if (value) {
+    gb->f |= (1 << 7);
+  } else {
+    gb->f &= ~(1 << 7);
+  }
+  gb->f &= 0xF0;
+}
+// Subtraction Flag
+void set_N(gb_core *gb, bool value) {
+  if (value) {
+    gb->f |= (1 << 6);
+  } else {
+    gb->f &= ~(1 << 6);
+  }
+  gb->f &= 0xF0;
+}
+// Half-Carry Flag
+void set_H(gb_core *gb, bool value) {
+  if (value) {
+    gb->f |= (1 << 5);
+  } else {
+    gb->f &= ~(1 << 5);
+  }
+  gb->f &= 0xF0;
+}
+// Carry Flag
+void set_C(gb_core *gb, bool value) {
+  if (value) {
+    gb->f |= (1 << 4);
+  } else {
+    gb->f &= ~(1 << 4);
+  }
+  gb->f &= 0xF0;
+}
+
+// Helpers
 uint8_t *read_binary_file(const char *path, uint32_t *out_size) {
 
   FILE *f = fopen(path, "rb");
@@ -72,6 +212,3 @@ uint8_t *read_binary_file(const char *path, uint32_t *out_size) {
 
   return buf;
 }
-
-void gb_step(gb_core *gb) { printf("Taking a step....\n"); }
-void gb_run_frame(gb_core *gb) { printf("Running a frame....\n"); }
